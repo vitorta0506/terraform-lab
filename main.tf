f657125b43199506d54295da55912791a045ec67
“vitortalves” <vitor.teixeira@vtainfo.com.br>
2020-05-27 11:57:45 -0300

    Ajuste nos arquivos

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region  = "${var.rgdefault}"
+  version = "~> 2.0"
+}
+
+terraform {
+  backend "s3" {
+    # Lembre de trocar o bucket para o seu, não pode ser o mesmo nome
+    bucket = "hebstatetf"
+    key    = "s3.us-east-2.amazonaws.com/hebstatetf"
+    region = "us-east-2"
+  }
+}
